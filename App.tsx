de88c9e7bee05f0bab137a74abf1202d129063a8
Jhulia Nogueira <jhulia.nogueira@pier.digital>
2022-01-07 17:20:43 -0400

    add tab navigation and dealing with native buttons

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
 
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
- } from '@expo-google-fonts/poppins';
+} from "@expo-google-fonts/poppins";
 
-import theme from './src/global/styles/theme';
+import theme from "./src/global/styles/theme";
 
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./src/routes/app.routes";
+
+import { Register } from "./src/screens/Register";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,13 +22,15 @@ export default function App() {
     Poppins_500Medium,
     Poppins_700Bold,
   });
-  if(!fontsLoaded){
-    return <AppLoading />
+  if (!fontsLoaded) {
+    return <AppLoading />;
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
-  )
+  );
 }
